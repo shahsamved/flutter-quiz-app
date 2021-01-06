1d8fb6d67d9bfd492c220c3f81194481d5f0d7c3
Samved <shahsamved@gmail.com>
2021-01-06 22:15:31 +0530

    Basic quiz app questions and options button

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  void ansQues() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = ['Whats ur fav color?'];
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('My 1st app'),
+          ),
+          body: Column(
+            children: [
+              Text('The question!'),
+              RaisedButton(
+                child: Text('Answer 1 '),
+                onPressed: ansQues,
+              ),
+              RaisedButton(
+                child: Text('Answer 2 '),
+                onPressed: ansQues,
+              ),
+              RaisedButton(
+                child: Text('Answer 3 '),
+                onPressed: ansQues,
+              )
+            ],
+          )),
     );
   }
 }
